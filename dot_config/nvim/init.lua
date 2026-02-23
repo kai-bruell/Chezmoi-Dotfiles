7220fb820136f55f17d3bb0172ece87a03e8a803
user <user@fedora.fritz.box>
2026-02-23 12:07:32 +0100

    feat: clipboard support for nvim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -11,3 +11,6 @@ vim.cmd('highlight NormalNC guibg=#000000 ctermbg=0')
 -- Graue Statusleiste
 vim.cmd('highlight StatusLine guibg=#3a3a3a guifg=#ffffff ctermbg=237 ctermfg=15')
 vim.cmd('highlight StatusLineNC guibg=#2a2a2a guifg=#808080 ctermbg=235 ctermfg=244')
+
+-- Clipboard Support
+vim.opt.clipboard = "unnamedplus"
