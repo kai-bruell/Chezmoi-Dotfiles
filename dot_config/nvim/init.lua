6b91a801b47aab4e30f6a0e80ad5f1de003a4a2e
kai-bruell <kaibruell@proton.me>
2026-07-25 22:12:54 +0200

    design: transparent status bar

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -8,9 +8,12 @@ vim.cmd('highlight Normal guibg=#000000 ctermbg=0')
 vim.cmd('highlight NonText guibg=#000000 ctermbg=0')
 vim.cmd('highlight NormalNC guibg=#000000 ctermbg=0')
 
--- Graue Statusleiste
-vim.cmd('highlight StatusLine guibg=#3a3a3a guifg=#ffffff ctermbg=237 ctermfg=15')
-vim.cmd('highlight StatusLineNC guibg=#2a2a2a guifg=#808080 ctermbg=235 ctermfg=244')
+-- Transparente Statusleiste
+vim.cmd('highlight StatusLine guibg=#3a3a3a guifg=#ffffff ctermbg=NONE ctermfg=15')
+vim.cmd('highlight StatusLineNC guibg=#2a2a2a guifg=#808080 ctermbg=NONE ctermfg=244')
+
+vim.cmd('colorscheme habamax')
+
 
 -- Clipboard Support
 vim.opt.clipboard = "unnamedplus"
