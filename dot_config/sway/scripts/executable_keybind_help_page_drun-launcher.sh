c2e81a09a47b7fdb0a6d93fa128584f3faa5d189
widepatch <kai.bruell@tech>
2026-07-30 02:14:13 +0200

    tmux/sway: 2-Level-Rofi-Hilfe mit Esc→Zurück und Kategorien

--- dot_config/sway/scripts/executable_keybind_help_page_drun-launcher.sh
+++ dot_config/sway/scripts/executable_keybind_help_page_drun-launcher.sh
@@ -3,13 +3,32 @@
 CONFIG_FILE="$HOME/.config/sway/config"
 THEME="$HOME/.config/sway/scripts/launcher.rasi"
 
-# 1. Config parsen: Zeilennummer, Keybinding und Kommentar (#) extrahieren
-parsed_list=$(awk '
-    /^[ \t]*bindsym/ {
+categories=$(awk '
+    /^### / {
+        gsub(/^###[ \t]+/, "")
+        cat = $0
+        next
+    }
+    /^[ \t]*bindsym/ && cat != "" {
+        if (!seen[cat]++) print cat
+    }
+' "$CONFIG_FILE")
+
+[ -z "$categories" ] && exit 0
+
+selected_cat=$(echo "$categories" | rofi -dmenu -i -p "Sway Keybindings" -theme "$THEME")
+[ -z "$selected_cat" ] && exit 0
+
+entries=$(awk -v cat="$selected_cat" '
+    BEGIN { in_cat = 0 }
+    /^### / {
+        gsub(/^###[ \t]+/, "")
+        in_cat = ($0 == cat)
+        next
+    }
+    /^[ \t]*bindsym/ && in_cat {
         line_num = NR
         sub(/^[ \t]*bindsym[ \t]+/, "")
-        
-        # Falls ein Kommentar in derselben Zeile steht
         if (match($0, /#[ \t]*/)) {
             binding = substr($0, 1, RSTART - 1)
             desc = substr($0, RSTART + RLENGTH)
@@ -17,32 +36,24 @@ parsed_list=$(awk '
             binding = $0
             desc = "No description"
         }
-        
-        # Formatierung säubern
         gsub(/[ \t]+$/, "", binding)
         gsub(/^[ \t]+|[ \t]+$/, "", desc)
-        
-        # Output: LineNumber | Binding | Description
         printf "%d\t%-30s\t# %s\n", line_num, binding, desc
     }
 ' "$CONFIG_FILE")
 
-if [ -z "$parsed_list" ]; then
-    notify-send "Sway Help" "Keine Keybindings in $CONFIG_FILE gefunden."
-    exit 1
-fi
+[ -z "$entries" ] && exec "$0"
 
-# 2. Rofi Auswahlfenster (Spalte A: Binding, Spalte B: Description)
-selected_line=$(echo "$parsed_list" | awk -F'\t' '{print $2 " │ " $3}' | rofi -dmenu -i -p "Keybindings Help" -theme "$THEME")
-
+selected_line=$(echo "$entries" | awk -F'\t' '{print $2 " │ " $3}' | rofi -dmenu -i -p "$selected_cat" -theme "$THEME")
+exit_code=$?
+[ $exit_code -eq 1 ] && exec "$0"
 [ -z "$selected_line" ] && exit 0
 
-# 3. Ausgewählte Zeilennummer und das reine Keybinding ermitteln
 binding_text=$(echo "$selected_line" | awk -F'│' '{print $1}' | xargs)
-line_num=$(echo "$parsed_list" | grep -F "$binding_text" | head -n1 | awk -F'\t' '{print $1}')
+line_num=$(echo "$entries" | grep -F "$binding_text" | head -n1 | awk -F'\t' '{print $1}')
 
-# 4. Aktionsmenü (Copy / Open in Nvim)
 action=$(printf "1. Copy Keybinding\n2. Open in Nvim (Line %s)" "$line_num" | rofi -dmenu -i -p "Action" -theme "$THEME")
+[ -z "$action" ] && exit 0
 
 case "$action" in
     "1. Copy Keybinding"*)
---
+++ dot_config/sway/scripts/executable_tmux-keybind-help.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+CONFIG_FILE="$HOME/.tmux.conf"
+THEME="$HOME/.config/sway/scripts/launcher.rasi"
+
+categories=$(awk '
+    /^# \[\[BLOCK:/ {
+        gsub(/^# \[\[BLOCK: |\]\]$/, "")
+        block_name = $0
+        has_cmd = 0
+        next
+    }
+    /^# \[\[\/BLOCK\]\]/ {
+        if (has_cmd) print block_name
+        block_name = ""
+        next
+    }
+    block_name != "" && /^# cmd:/ {
+        has_cmd = 1
+    }
+' "$CONFIG_FILE")
+
+[ -z "$categories" ] && exit 0
+
+selected_cat=$(echo "$categories" | rofi -dmenu -i -p "Tmux Keybindings" -theme "$THEME")
+[ -z "$selected_cat" ] && exit 0
+
+entries=$(awk -v cat="$selected_cat" '
+    BEGIN { in_block = 0 }
+    /^# \[\[BLOCK:/ {
+        gsub(/^# \[\[BLOCK: |\]\]$/, "")
+        in_block = ($0 == cat)
+        next
+    }
+    /^# \[\[\/BLOCK\]\]/ { in_block = 0 }
+    in_block && /^# cmd:/ {
+        match($0, /^# cmd:[ \t]+([^|]+)[ \t]*\|[ \t]*desc:[ \t]+(.+)$/, m)
+        if (m[1] != "") {
+            gsub(/^[ \t]+|[ \t]+$/, "", m[1])
+            gsub(/^[ \t]+|[ \t]+$/, "", m[2])
+            printf "%-15s │ %s\n", m[1], m[2]
+        }
+    }
+' "$CONFIG_FILE")
+
+[ -z "$entries" ] && exec "$0"
+
+selected_entry=$(echo "$entries" | rofi -dmenu -i -p "$selected_cat" -theme "$THEME")
+exit_code=$?
+[ $exit_code -eq 1 ] && exec "$0"
+[ -z "$selected_entry" ] && exit 0
+
+key=$(echo "$selected_entry" | awk -F'│' '{print $1}' | xargs)
+
+action=$(printf "1. Copy Keybinding\n2. Open in Nvim" | rofi -dmenu -i -p "Action" -theme "$THEME")
+[ -z "$action" ] && exit 0
+
+case "$action" in
+    "1. Copy Keybinding"*)
+        echo -n "$key" | wl-copy
+        notify-send "Tmux Help" "Kopiert: $key"
+        ;;
+    "2. Open in Nvim"*)
+        line_num=$(grep -nF "# cmd: ${key} |" "$CONFIG_FILE" | head -1 | cut -d: -f1)
+        foot -e nvim +"$line_num" "$CONFIG_FILE"
+        ;;
+esac
